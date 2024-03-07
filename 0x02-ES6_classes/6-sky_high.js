6eff92894bfac53e4dfb353feb34f82763a8086a
vincent-mugendi <vincentmwathi5@gmail.com>
2024-03-07 16:59:26 +0300

    Added 6-sky_high.js and test

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+// 6-sky_high.js
+import Building from './5-building.js';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // Override evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
+
+export default SkyHighBuilding;
