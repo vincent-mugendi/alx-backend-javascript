4a0d0ece45a1387ebb7ee59e8e0faa10fb86cce0
vincent-mugendi <vincentmwathi5@gmail.com>
2024-03-07 17:37:15 +0300

    Updtaes to 0x00. ES6 Basics Project

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,6 @@
+//  function named iterateThroughObject. The function’s parameter
+// reportWithIterator is the return value from createIteratorObject.
+// return every employee name in a string, separated by |
+export default function iterateThroughObject(reportWithIterator) {
+  return reportWithIterator.join(' | ');
+}
---
+++ 0x00-ES6_basic/README.md
@@ -0,0 +1,3 @@
+# 0x00-ES6_basic
+
+# Description
