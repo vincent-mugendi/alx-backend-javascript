7778da262f963adca77c4c848f8deebfa3e39052
vincent-mugendi <vincentmwathi5@gmail.com>
2025-04-02 02:48:37 +0300

    Task Number 2: -Fixing lint issues

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,10 +2,10 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.question("Welcome to Holberton School, what is your name?\n", (userName) => {
+rl.question('Welcome to Holberton School, what is your name?\n', (userName) => {
   console.log(`Your name is: ${userName}`);
 
   console.log('This important software is now closing');
