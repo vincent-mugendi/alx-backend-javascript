e2c041f7d374e09c1950348eaac6381935f5acf4
vincent-mugendi <vincentmwathi5@gmail.com>
2025-04-02 02:45:09 +0300

    Task Number 2: -Fixing identation issues

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
 rl.question("Welcome to Holberton School, what is your name?\n", (userName) => {
-    console.log(`Your name is: ${userName}`);
+  console.log(`Your name is: ${userName}`);
 
-    console.log('This important software is now closing\n');
-    rl.close();
+  console.log('This important software is now closing');
+  rl.close();
 });
