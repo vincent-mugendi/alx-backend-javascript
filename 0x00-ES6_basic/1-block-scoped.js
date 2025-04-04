7f258ebf33acd3508efd8949d9bcde8b637c34ec
vincent-mugendi <vincentmwathi5@gmail.com>
2025-04-04 03:30:27 +0300

    Task 1: Solved

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  let task;
-  let task2;
+  let task = false;
+  let task2 = true;
+
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    let task = true;
+    let task2 = false;
   }
-  task = false;
-  task2 = true;
+
   return [task, task2];
 }
